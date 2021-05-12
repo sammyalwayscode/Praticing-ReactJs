34d3d3de82dafed284151d0fe32502b814b7b124
unknown <Olorunda Samuel>
2021-05-12 12:46:59 +0100

    Various Pratices using ReactJS

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+// import React, { useContext } from "react";
+// import Alert from "./Components/Alert/Alert";
+// import { GlobalState, GlobalVariable } from "./Components/Alert/GlobalState";
+import GetMethod from "./Components/FireBaseMethods/GetMethod";
+import PostMethod from "./Components/FireBaseMethods/PostMethod";
+// import WordTest from "./Components/WordPpp/WordTest";
+// import SignsToogle from "./Components/SignInSignUpToogle/SignsToogle";
+// import PageOne from "./Components/MultiPageToggle/PageOne";
+// import Click from './Components/OnePageToogle/Click'
+
+function App() {
+  //const { show, open } = useContext(GlobalVariable);
+  return (
+    // <GlobalState>
+    <div>
+      {/* <Alert /> */}
+      {/* <Click /> */}
+      {/* <PageOne /> */}
+      {/* <SignsToogle /> */}
+      <PostMethod />
+      <GetMethod />
+      {/* <WordTest /> */}
+    </div>
+    // </GlobalState>
+  );
+}
+
+export default App;
